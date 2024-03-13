17bfd06e2c3489a65e4ac8456f168e81e8bdf0e7
Habil UZUNDAĞ <habiluzundag@gmail.com>
2024-03-14 01:44:15 +0300

    Progress bar added for read

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from pypdf import PdfReader
 from gtts import gTTS
+from tqdm import tqdm
 
 pdf_file = input("Seslendirmek istediğiniz Pdf dosyasının yolunu girin: ")
 sound_name = input("Kayıt adı girin: ")
 reader = PdfReader(pdf_file)
 text=""
-for page_no in range(len(reader.pages)):
+total_pages = len(reader.pages)
+
+for page_no in tqdm(range(total_pages), desc="Pdf okunuyor..."):
     page = reader.pages[page_no]
     text +=page.extract_text()
 sound = gTTS(text=text, lang='tr',slow=False)
+print("Ses kayıt ediliyor...")
 sound.save('%s.mp3'%sound_name)
